e559ba19ee905522d8551e91a4d0d935995e16c1
Aseenshaik786 <aseenshaik786@gmail.com>
2025-06-30 03:59:32 +0530

    Initial commit

---
+++ ArtworkTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VirtualArtGallery.DAO;
+using VirtualArtGallery.Entity;
+using VirtualArtGallery.Exception;
+
+namespace VirtualArtGallery.Tests
+{
+    [TestClass]
+    public class ArtworkTests
+    {
+        private IVirtualArtGallery service;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            service = new VirtualArtGalleryImpl();
+        }
+
+        [TestMethod]
+        public void TestAddArtwork()
+        {
+            var artwork = new Artwork(106, "Test", "Testing", "2024", "Digital", "img.png", 1);
+            bool result = service.AddArtwork(artwork);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArtWorkNotFoundException))]
+        public void TestGetInvalidArtwork()
+        {
+            service.GetArtworkById(999);
+        }
+    }
+}
